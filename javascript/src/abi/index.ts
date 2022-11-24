ae49dc174aa3eea43c42b73535bf5fa5a3069e57
tuannm <nminh.tuan106@gmail.com>
2022-11-24 15:42:47 +0700

    implement generate message to sign javascript

---
+++ javascript/src/abi/index.ts
@@ -0,0 +1,8 @@
+const AmendRequest = require("./AmendRequest.json");
+const LCManagement = require("./LCManagement.json");
+const Mode = require("./Mode.json");
+const RouterService = require("./RouterService.json");
+const StandardLCFactory = require("./StandardLCFactory.json");
+const UPASLCFactory = require("./UPASLCFactory.json");
+
+export const LCContractABIs = { AmendRequest, LCManagement, Mode, RouterService, StandardLCFactory, UPASLCFactory };
