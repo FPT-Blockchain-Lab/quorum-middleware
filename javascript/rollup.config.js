6e2a90ba8ea13ce006b90062546c382d6cf08253
tuannm <nminh.tuan106@gmail.com>
2022-12-02 16:17:59 +0700

    update: rollup config and tsconfig

--- javascript/rollup.config.js
+++ javascript/rollup.config.js
@@ -2,17 +2,15 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import nodePolyfills from "rollup-plugin-polyfill-node";
-import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 
 export default {
-    input: "src/middleware/index.ts",
+    input: "dist/middleware/index.js",
     output: {
-        file: "./dist.browser/index.js",
+        file: "dist.browser/index.js",
         format: "umd",
         name: "LC",
-        sourcemap: true,
     },
     context: "commonjsGlobal",
-    plugins: [commonjs(), resolve({ browser: true }), json(), nodePolyfills(), typescript({ module: "ESNext" }), terser()],
+    plugins: [commonjs(), resolve({ browser: true, dedupe: ["bn.js"] }), json(), nodePolyfills(), terser()],
 };
