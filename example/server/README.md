675e96214d6705a5cdf9dc7dc7a00ac1ffaf64dd
Truong Ma Phi <trmaphi@gmail.com>
2023-09-24 20:12:03 +0700

    fix: push exampe

---
+++ example/server/README.md
@@ -0,0 +1,5 @@
+# Example for LC protocol in Java
+```bash
+mvn install
+mvn compile exec:java -Dexec.mainClass="com.fptblockchainlab.example.App"
+```
---
+++ example/server/src/main/java/com/fptblockchainlab/example/App.java
@@ -0,0 +1,15 @@
+package com.fptblockchainlab.example;
+
+import com.fptblockchainlab.middleware.LC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(LC.DEFAULT_ROOT_HASH);
+    }
+}
