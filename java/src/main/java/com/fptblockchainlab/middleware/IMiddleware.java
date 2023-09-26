91292bc0d92aa89ba9e12f005a8161790b101a80
tuannm <nminh.tuan106@gmail.com>
2023-09-26 09:23:11 +0700

    wip: implement feature for LC contract

--- java/src/main/java/com/fptblockchainlab/middleware/IMiddleware.java
+++ java/src/main/java/com/fptblockchainlab/middleware/IMiddleware.java
@@ -12,25 +12,13 @@ import java.util.List;
 public interface IMiddleware {
 
     /**
-     * get admin role from default roles
-     * @return
-     */
-    Permission.Role getAdminRole();
-
-    /**
-     * Get member role from default role
-     * @return
-     */
-    Permission.Role getMemberRole();
-
-    /**
      * Give a level 1 org in MiddlewareSErvice check if account is active and under this level 1 org
      * or any sub or of this level 1 org
      * @param account
      * @return
      * @throws IOException
      */
-    boolean isAccountOnchainUnderLevel1Org(String account) throws IOException;
+//    boolean isAccountOnchainUnderLevel1Org(String account) throws IOException;
 
     /**
      *
@@ -40,14 +28,14 @@ public interface IMiddleware {
      * @throws TransactionException
      * @throws FailedTransactionException
      */
-    void signWithAdminAndSend(String to, String data) throws IOException, TransactionException, FailedTransactionException;
+//    void signWithAdminAndSend(String to, String data) throws IOException, TransactionException, FailedTransactionException;
 
     /**
      * sign message hash compatible with personal_sign
      * @param messageHash
      * @return
      */
-    public String signWithUltimateParentAdmin(String messageHash);
+//    public String signWithUltimateParentAdmin(String messageHash);
 
     /**
      * Create sub org for org level 1
@@ -56,12 +44,12 @@ public interface IMiddleware {
      * @throws FailedTransactionException
      * @throws IOException
      */
-    void createSubOrgWithDefaultRoles(String orgFullId) throws NotParentOrgException, FailedTransactionException, IOException;
+//    void createSubOrgWithDefaultRoles(String orgFullId) throws NotParentOrgException, FailedTransactionException, IOException;
 
     /**
      * Add
      */
-    void addAdminForSubOrg(String subOrgFullId, String adminWallet) throws IOException, FailedTransactionException;
+//    void addAdminForSubOrg(String subOrgFullId, String adminWallet) throws IOException, FailedTransactionException;
 
     /**
      * Allow org to use LC protocol
@@ -69,7 +57,7 @@ public interface IMiddleware {
      * @throws FailedTransactionException
      * @throws IOException
      */
-    void whiteListOrg(String orgFullId) throws FailedTransactionException, IOException;
+//    void whiteListOrg(String orgFullId) throws FailedTransactionException, IOException;
 
     /**
      * Restrict org to use LC protocol
@@ -77,12 +65,12 @@ public interface IMiddleware {
      * @throws FailedTransactionException
      * @throws IOException
      */
-    void unwhiteListOrg(String orgFullId) throws FailedTransactionException, IOException;
+//    void unwhiteListOrg(String orgFullId) throws FailedTransactionException, IOException;
 
     /**
      * Suspend admin of sub org
      */
-    void suspendAdminSubOrg(String subOrgFullId, String adminAddress) throws FailedTransactionException, IOException;
+//    void suspendAdminSubOrg(String subOrgFullId, String adminAddress) throws FailedTransactionException, IOException;
 
     void createStandardLC(List<String> parties, String prevHash, String[] contentHash, String url, BigInteger signedTime, int numOfDocuments, String acknowledge, String privateKey) throws FailedTransactionException, IOException;
 
